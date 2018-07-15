fb65dfbcd0d3f3c90c6fed8215a4331931ee1f84
Jeisson Hidalgo <jeissonh@gmail.com>
2018-07-14 21:32:48 -0600

    Game menu: controls to setup a multiplayer game

--- src/GameMenuView.cpp
+++ src/GameMenuView.cpp
@@ -6,7 +6,15 @@ GameMenuView::GameMenuView(QWidget* parent)
 	, ui(new Ui::GameMenuView)
 {
 	// Create the user interface created with QtDesiner
-	ui->setupUi(this);
+	this->ui->setupUi(this);
+
+	// Try to enable the Start button when a GUI control is activated
+	this->connect( ui->radioButtonSinglePlayer, &QRadioButton::clicked, this, &GameMenuView::tryEnableStartButton );
+	this->connect( ui->radioButtonMultiplayer, &QRadioButton::clicked, this, &GameMenuView::tryEnableStartButton );
+	this->connect( ui->radioButtonStartMatch, &QRadioButton::clicked, this, &GameMenuView::tryEnableStartButton );
+	this->connect( ui->radioButtonJoinMatch, &QRadioButton::clicked, this, &GameMenuView::tryEnableStartButton );
+	this->connect( ui->lineEditHostname, &QLineEdit::textChanged, this, &GameMenuView::tryEnableStartButton );
+	this->connect( ui->lineEditPort, &QLineEdit::textChanged, this, &GameMenuView::tryEnableStartButton );
 
 	// When Quit button is pressed, close the MainWindow (parent)
 	this->connect( ui->pushButtonQuit, &QPushButton::clicked, parent, &QWidget::close );
@@ -14,5 +22,42 @@ GameMenuView::GameMenuView(QWidget* parent)
 
 GameMenuView::~GameMenuView()
 {
-	delete ui;
+	delete this->ui;
+}
+
+bool GameMenuView::canEnableStartButton() const
+{
+	// When we are in the initial state
+	if ( this->state == GameMenuState::initial )
+	{
+		// A player's nickname is mandatory
+		if ( ui->lineEditNickname->text().trimmed().length() <= 0 ) return false;
+
+		// The nickname is the only data required for a single player game
+		if ( ui->radioButtonSinglePlayer->isChecked() ) return true;
+
+		// The multiplayer option is selected
+		// In both, client and server the port is required
+		if ( ! ( ui->lineEditPort->text().toInt() > 1024 ) ) return false;
+
+		// In client mode we also require the hostname
+		if ( ui->radioButtonJoinMatch->isChecked() && ui->lineEditHostname->text().trimmed().length() <= 0 )
+			return false;
+
+		// We do not find any subjections to disable the Start button
+	}
+
+	// For a multiplayer game, the button will managed later
+	return true;
+}
+
+void GameMenuView::on_lineEditNickname_textChanged(const QString& text)
+{
+	Q_UNUSED(text);
+	this->tryEnableStartButton();
+}
+
+void GameMenuView::tryEnableStartButton()
+{
+	this->ui->pushButtonStart->setEnabled( canEnableStartButton() );
 }
--- src/GameMenuView.h
+++ src/GameMenuView.h
@@ -9,6 +9,16 @@ namespace Ui
 }
 
 /**
+	The states of the game menu before a game starts
+*/
+enum class GameMenuState
+{
+	initial,
+	waitingForClients,
+	connectingToServer,
+};
+
+/**
 	The game menu. It is shown in the MainWindow's central widget when the game is in game menu
 	state, according to the state machine.
 **/
@@ -16,15 +26,25 @@ class GameMenuView : public QWidget
 {
 	Q_OBJECT
 
+  private:
+	/// A record that contains the user interface designed by QtDesigner
+	Ui::GameMenuView* ui = nullptr;
+	/// The state of the game menu view
+	GameMenuState state = GameMenuState::initial;
+
   public:
 	/// Constructor
 	explicit GameMenuView(QWidget* parent = nullptr);
 	/// Destructor
-	~GameMenuView();
+	virtual ~GameMenuView();
+	/// Returns true if the conditions to enable the Start button are met
+	bool canEnableStartButton() const;
 
-  private:
-	/// A record that contains the user interface designed by QtDesigner
-	Ui::GameMenuView *ui;
+  private slots:
+	/// Called each time the user changes the nickname text in order to enable the Start button
+	void on_lineEditNickname_textChanged(const QString &text);
+	/// If the conditions to enable the Start button are met, enables it
+	void tryEnableStartButton();
 };
 
 #endif // GAMEMENUVIEW_H
